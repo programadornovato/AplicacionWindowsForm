6c022eef5ef9151d6a7348ed292e71735d24ff73
Programador Novato <info@programadornovato.com>
2021-06-14 00:27:32 -0500

    Propiedades de las ventanas en C# [02]

--- AplicacionWindowsForm/Form1.Designer.cs
+++ AplicacionWindowsForm/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace AplicacionWindowsForm
 {
-    partial class Form1
+    partial class Formulario1
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,27 +29,66 @@ namespace AplicacionWindowsForm
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario1));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(579, 272);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(140, 201);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // button2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.button2.Location = new System.Drawing.Point(140, 127);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Formulario1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 433);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::AplicacionWindowsForm.Properties.Resources.logo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(684, 577);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(500, 500);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Name = "Formulario1";
+            this.Opacity = 0.7D;
+            this.Text = "Formulario 1";
             this.ResumeLayout(false);
 
         }
@@ -57,6 +96,8 @@ namespace AplicacionWindowsForm
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- AplicacionWindowsForm/Form1.cs
+++ AplicacionWindowsForm/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AplicacionWindowsForm
 {
-    public partial class Form1 : Form
+    public partial class Formulario1 : Form
     {
-        public Form1()
+        public Formulario1()
         {
             InitializeComponent();
         }
