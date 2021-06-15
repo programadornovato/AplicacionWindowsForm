436cbb3a41899bc47be81614318b30e398c30df5
Programador Novato <info@programadornovato.com>
2021-06-15 18:57:21 -0500

    DatagridView en C# [08]

--- AplicacionWindowsForm/Ventana2.Designer.cs
+++ AplicacionWindowsForm/Ventana2.Designer.cs
@@ -29,52 +29,147 @@ namespace AplicacionWindowsForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pbCarga = new System.Windows.Forms.ProgressBar();
-            this.lblPosicion = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.dgPersonas = new System.Windows.Forms.DataGridView();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEdad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.lblValor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPersonas)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // dgPersonas
             // 
-            this.pictureBox1.Image = global::AplicacionWindowsForm.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(152, 89);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(282, 277);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.dgPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPersonas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNombre,
+            this.colApellido,
+            this.colEdad});
+            this.dgPersonas.Location = new System.Drawing.Point(40, 200);
+            this.dgPersonas.Name = "dgPersonas";
+            this.dgPersonas.RowTemplate.Height = 25;
+            this.dgPersonas.Size = new System.Drawing.Size(355, 150);
+            this.dgPersonas.TabIndex = 5;
+            this.dgPersonas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPersonas_CellClick);
             // 
-            // pbCarga
+            // colNombre
             // 
-            this.pbCarga.Location = new System.Drawing.Point(152, 420);
-            this.pbCarga.Name = "pbCarga";
-            this.pbCarga.Size = new System.Drawing.Size(282, 40);
-            this.pbCarga.TabIndex = 1;
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.Name = "colNombre";
             // 
-            // lblPosicion
+            // colApellido
             // 
-            this.lblPosicion.AutoSize = true;
-            this.lblPosicion.Location = new System.Drawing.Point(152, 503);
-            this.lblPosicion.Name = "lblPosicion";
-            this.lblPosicion.Size = new System.Drawing.Size(65, 25);
-            this.lblPosicion.TabIndex = 2;
-            this.lblPosicion.Text = "label1";
+            this.colApellido.HeaderText = "Apellido";
+            this.colApellido.Name = "colApellido";
+            // 
+            // colEdad
+            // 
+            this.colEdad.HeaderText = "Edad";
+            this.colEdad.Name = "colEdad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(152, 36);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 33);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(152, 79);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(200, 33);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(152, 120);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(200, 33);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(421, 39);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(105, 30);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(421, 95);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(105, 30);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(40, 377);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(56, 25);
+            this.lblValor.TabIndex = 1;
+            this.lblValor.Text = "Edad";
             // 
             // Ventana2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 589);
-            this.Controls.Add(this.lblPosicion);
-            this.Controls.Add(this.pbCarga);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgPersonas);
             this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Name = "Ventana2";
             this.Text = "Ventana2";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPersonas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +177,18 @@ namespace AplicacionWindowsForm
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ProgressBar pbCarga;
-        private System.Windows.Forms.Label lblPosicion;
+        private System.Windows.Forms.DataGridView dgPersonas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colApellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEdad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Label lblValor;
     }
 }
--- AplicacionWindowsForm/Ventana2.cs
+++ AplicacionWindowsForm/Ventana2.cs
@@ -11,31 +11,40 @@ namespace AplicacionWindowsForm
     public partial class Ventana2 : Form
     {
         int con = 0;
+        int sel;
         public Ventana2()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void btnAgregar_Click(object sender, EventArgs e)
         {
-            pbCarga.Minimum = 0;
-            pbCarga.Maximum = 100;
-            if (con < 100)
-            {
-                con++;
-            }
-            pbCarga.Value = con;
+            int n = dgPersonas.Rows.Add();
+            dgPersonas.Rows[n].Cells[0].Value = txtNombre.Text;
+            dgPersonas.Rows[n].Cells[1].Value = txtApellido.Text;
+            dgPersonas.Rows[n].Cells[2].Value = txtEdad.Text;
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtEdad.Text = "";
         }
 
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        private void dgPersonas_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            lblPosicion.Text = "X="+e.X+" Y="+e.Y;
-            pbCarga.Minimum = 0;
-            pbCarga.Maximum = 100;
-            if (con < 100) {
-                con++;
+            sel = e.RowIndex;
+            if (sel != -1) {
+                string nombre = dgPersonas.Rows[sel].Cells[0].Value.ToString();
+                string apellido = dgPersonas.Rows[sel].Cells[1].Value.ToString();
+                string edad = dgPersonas.Rows[sel].Cells[2].Value.ToString();
+                lblValor.Text = nombre +" "+ apellido + " " + edad;
             }
-            pbCarga.Value = con;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (sel != -1)
+            {
+                dgPersonas.Rows.RemoveAt(sel);
+            }
         }
     }
 }
